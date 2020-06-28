0554ff1f0e7aa98283a615746e13b54b5cd1b088
rajne123 <31688559+rajne123@users.noreply.github.com>
2020-06-28 16:40:30 +0530

    Initial commit

---
+++ JsPage1.js
@@ -0,0 +1,30 @@
+var itemCounter = 1;
+var vHeadLine = document.getElementById("RS1") ;
+var Ulvaritems = document.getElementById("ULRS1").getElementsByTagName("li");
+var vButton = document.getElementById("BT1");
+var vOuterList = document.getElementById("ULRS1");
+
+vButton.addEventListener("click" , addnewListItems);
+vOuterList.addEventListener("click" , callfunc1);
+
+//Adding Listener at OuterList level
+
+/*
+for (i = 0 ;  i < Ulvaritems.length ; i++ )
+{
+  //Ulvaritems[i].innerHTML = i ///
+  Ulvaritems[i].addEventListener("click" , callfunc1);
+}
+*/
+
+function callfunc1(e)
+{
+  vHeadLine.innerHTML = e.target.innerHTML;
+  //this.classlist.add("active");
+}
+
+function addnewListItems ()
+{
+vOuterList.innerHTML += "<li>" + "Something new" + itemCounter + "</li>" ;
+itemCounter++ ;
+}
