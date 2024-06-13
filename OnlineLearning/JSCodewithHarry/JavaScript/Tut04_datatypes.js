88eaffa8adf8d334488866adb50a8b4bfab1de8f
rajne123 <31688559+rajne123@users.noreply.github.com>
2024-06-13 14:11:33 +0530

    Tut04 updated

--- OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
+++ OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
@@ -1,5 +1,9 @@
 let a = null ; 
 let b = undefined ; 
 let c = BigInt(240); 
+let f = Symbol("Hello i m Symbol");
 
 console.log ("Value of a : " , a, b , c) ; 
+/* Find out type of data type */
+
+console.log("type of c",typeof c, "/n" ,  "Type of f:",typeof f); 
