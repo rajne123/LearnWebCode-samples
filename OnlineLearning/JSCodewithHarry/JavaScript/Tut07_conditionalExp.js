98cca5b0884b2d36226f560be1b09f2c3906ce12
rajne123 <31688559+rajne123@users.noreply.github.com>
2024-06-13 17:36:05 +0530

    updated

--- OnlineLearning/JSCodewithHarry/JavaScript/Tut07_conditionalExp.js
+++ OnlineLearning/JSCodewithHarry/JavaScript/Tut07_conditionalExp.js
@@ -1,4 +1,13 @@
 const prompt = require('prompt-sync')({sigint: true});
 let a = prompt("What's your name :: ");
 
+/*converting to Integer*/
+a = Number.parseInt(a) ; 
 console.log(a);
+
+if (a > 18) {
+    console.log ("You are eligible to drive")
+}
+else{
+    console.log ("Sorry , You are too young , you got to WAIT")1
+}
