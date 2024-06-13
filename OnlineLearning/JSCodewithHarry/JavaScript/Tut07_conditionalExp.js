92ac5c3f16d02ea4caced2e0b8984e189e660aea
rajne123 <31688559+rajne123@users.noreply.github.com>
2024-06-13 17:38:46 +0530

    updated

--- OnlineLearning/JSCodewithHarry/JavaScript/Tut07_conditionalExp.js
+++ OnlineLearning/JSCodewithHarry/JavaScript/Tut07_conditionalExp.js
@@ -1,3 +1,8 @@
+/*to use "Prompt" function in nodejs you have to include package "prompt-sync. 
+to do that first install package by :
+npm install promtp-sync*
+Thereafter you got to include the package/library by "require" command as given below */
+
 const prompt = require('prompt-sync')({sigint: true});
 let a = prompt("What's your name :: ");
 
@@ -5,6 +10,8 @@ let a = prompt("What's your name :: ");
 a = Number.parseInt(a) ; 
 console.log(a);
 
+
+/*Note : Nodejs doesn't have ALERT function .. so instead use console.log */
 if (a > 18) {
     console.log ("You are eligible to drive")
 }
