46930dee7e45694cf8668bd92973462477fdcb59
rajne123 <31688559+rajne123@users.noreply.github.com>
2024-06-13 14:21:59 +0530

    updated

--- OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
+++ OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
@@ -2,8 +2,10 @@ let a = null ;
 let b = undefined ; 
 let c = BigInt(240); 
 let f = Symbol("Hello i m Symbol");
+let RSbool = true; 
+let RSString = "hello i am string"
 
-console.log ("Value of a : " , a, b , c) ; 
+console.log ("Value of a : " , a, '\n', b , '\n', c , '\n', f , '\n', RSbool , '\n', RSString) ; 
 /* Find out type of data type */
 
-console.log("type of c",typeof c, "/n" ,  "Type of f:",typeof f); 
+console.log("type of c:",typeof c, '\n' ,  "Type of f:",typeof f); 
