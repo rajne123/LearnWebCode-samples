222f05a8390ba45f9e7dc1683ab978aa5bac3a4c
rajne123 <31688559+rajne123@users.noreply.github.com>
2024-06-13 14:30:04 +0530

    Updated with Object example

--- OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
+++ OnlineLearning/JSCodewithHarry/JavaScript/Tut04_datatypes.js
@@ -9,3 +9,16 @@ console.log ("Value of a : " , a, '\n', b , '\n', c , '\n', f , '\n', RSbool , '
 /* Find out type of data type */
 
 console.log("type of c:",typeof c, '\n' ,  "Type of f:",typeof f); 
+
+/*Object JS - Key-Value Pairs*/
+const RSlist = {
+"UP" : "Lucknow" ,
+"bool1" : "true" ,
+"Rajneesh" : "45" ,
+"Inventure" : "IGCSE"
+}
+
+console.log("Value of Object:",RSlist["UP"]) ;
+console.log("Value of Object:",RSlist["Inventure"]) ;
+console.log("Value of Object:",RSlist["bool1"]) ;
+console.log("Value of Object:",RSlist["Junk1"]) ;
